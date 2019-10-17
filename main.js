bd7e0c84348e64a5e889dfb0d1913d56aa1b6ccb
Patrik Vácal <patrik.vacal@gmail.com>
2019-10-17 14:58:48 +0200

    Created basic Firefox extension to eliminate ctrl+s / cmd+s

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown", function(e) {
+    if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)  && e.keyCode == 83) {
+      e.preventDefault();
+      // Process the event here (such as click on submit button)
+    }
+  }, false);
